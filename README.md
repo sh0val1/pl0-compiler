debe06738b56ce20081f11fa736e05efe98644a4
sh0val1 <shovalihagah2004@gmail.com>
2025-06-22 21:35:19 +0300

    start writing lexer code; skip_comment function

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pl0-compiler
 A pl/0 to C compiler written in C
+
+
+TBD list:
+1. lexer that parses tokens
+2. parser that parses the order of the tokens
+3. code generator that'll generate result c code 
+
+Also TBD for styling:
+1. Divide to files for each component and write makefile, create a binary out of multiple files
--- compiler.c
+++ compiler.c
@@ -3,8 +3,40 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+// pl0 possible token types
+#define TOK_IDENT       'I'
+#define TOK_NUMBER	    'N'
+#define TOK_CONST	    'C'
+#define TOK_VAR		    'V'
+#define TOK_PROCEDURE	'P'
+#define TOK_CALL	    'c'
+#define TOK_BEGIN	    'B'
+#define TOK_END		    'E'
+#define TOK_IF		    'i'
+#define TOK_THEN	    'T'
+#define TOK_WHILE	    'W'
+#define TOK_DO		    'D'
+#define TOK_ODD		    'O'
+#define TOK_DOT		    '.'
+#define TOK_EQUAL	    '='
+#define TOK_COMMA	    ','
+#define TOK_SEMICOLON	';'
+#define TOK_ASSIGN	    ':' // Assignment is :=, but here it's one char for convention
+#define TOK_HASH	    '#'
+#define TOK_LESSTHAN	'<'
+#define TOK_GREATERTHAN	'>'
+#define TOK_PLUS        '+'
+#define TOK_MINUS	    '-'
+#define TOK_MULTIPLY    '*'
+#define TOK_DIVIDE	    '/'
+#define TOK_LPAREN	    '('
+#define TOK_RPAREN	    ')'
+#define TOK_LCOMM       '{'
+#define TOK_RCOMM       '{'
+
 
 char *raw_file_data;  // supposed to contain the .pl0 file raw data as string
+int *offset_in_file;
 
 void print_error(char *error_msg){
     printf(error_msg);
