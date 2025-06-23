2a55dd91da889aa53fcd19aba4a1eeb5ce964fb6
sh0val1 <shovalihagah2004@gmail.com>
2025-06-24 01:19:49 +0300

    add lex() function for recognizing tokens and all its dependencies

--- compiler.c
+++ compiler.c
@@ -38,8 +38,9 @@
 
 char *raw_file_data;  // supposed to contain the .pl0 file raw data as string
 int raw_file_data_len = 0;
-int *curr_index = 0;
+char *token;
 int curr_row = 0;
+int line = 0;  // TODO: use in order to print line number as error.
 
 void print_error(char *error_msg) {
     printf(error_msg);
@@ -78,10 +79,10 @@ static void read_file(char *file_path) {
     close(fd);
 }
 
-int skip_comment() {
-    for (int i = curr_index; i < raw_file_data_len; i++) {
+int comment() {
+    for (int i = raw_file_data; i < raw_file_data_len; i++) {
         if (raw_file_data[i] == TOK_RCOMM)
-            curr_index = i;
+            raw_file_data++;
             return 0;
     }
     print_error("Could not find end of comment");
@@ -91,7 +92,7 @@ char* get_function_name() {
     char *function_name[100];
     int function_index = 0;
     char current_char;
-    for (int i = curr_index; i < raw_file_data_len; i++) {
+    for (int i = raw_file_data; i < raw_file_data_len; i++) {
         current_char = raw_file_data[i];
         if (isalpha(current_char)) {
             function_name[function_index] = current_char;
@@ -99,7 +100,7 @@ char* get_function_name() {
         else { 
             if (current_char == ";") {
                 function_name[i] = "/0";
-                curr_index = i;
+                raw_file_data = i;
                 return function_name;
             }
             else {
@@ -108,6 +109,88 @@ char* get_function_name() {
         }
     }
     print_error("Non-terminated procedure name.");
+}
+
+int ident() {
+    char *original_pos = raw_file_data;
+    while (isalnum(*raw_file_data) || *raw_file_data == "_")
+        raw_file_data++;
+
+    size_t len = raw_file_data - original_pos;
+    free(token);
+    if (token = malloc(len + 1) == NULL) {
+        print_error("Failed allocating memory for token");
+    }
+
+    size_t i;
+    for (i = 0; i < len; i++)
+        token[i] = *original_pos++;
+    token[i] = "\0";
+
+    if(!strcmp(token, "const"))
+        return TOK_CONST;
+    else if (!strcmp(token, "var"))
+        return TOK_VAR;
+    else if (!strcmp(token, "procedure"))
+        return TOK_PROCEDURE;
+    else if (!strcmp(token, "call"))
+        return TOK_CALL;
+    else if (!strcmp(token, "begin"))
+        return TOK_BEGIN;
+    else if (!strcmp(token, "end"))
+        return TOK_END;
+    else if (!strcmp(token, "if"))
+        return TOK_IF;
+    else if (!strcmp(token, "then"))
+        return TOK_THEN;
+    else if (!strcmp(token, "while"))
+        return TOK_WHILE;
+    else if (!strcmp(token, "do"))
+        return TOK_DO;
+    else if (!strcmp(token, "odd"))
+        return TOK_ODD;
+    return TOK_IDENT; // some string to be recognized; function name, variablle name, etc.
+}
+
+int number() {
+    char *original_pos = raw_file_data;
+    while (isdigit(*raw_file_data) || *raw_file_data == "_")
+        raw_file_data++;
+
+    size_t len = raw_file_data - original_pos;
+    free(token);
+    if (token = malloc(len + 1) == NULL) {
+        print_error("Failed allocating memory for token");
+    }
+
+    size_t i;
+    for (i = 0; i < len; i++)
+        if (isdigit(*original_pos))
+            token[i] = *original_pos++;
+    token[i] = "\0";
+
+    return TOK_NUMBER;
+}
+
+int lex() {
+    while(*raw_file_data == ' ' || *raw_file_data == '\t' || *raw_file_data == '\n') {
+        if (*raw_file_data == '\n')
+            line++;
+        raw_file_data++;
+    }
+
+    if (isalnum(*raw_file_data)) {
+        return ident();
+    }
+
+    if (isdigit(*raw_file_data)) {
+        return number();
+    }
+    // cases: 1. some string (id, const, var, procedure etc.) 2. expressions 3. number
+}
+
+void parse() {
+    // TODO: get lex() token and print it for now
 }
 
 int main(int argc, char *argv[]) {
